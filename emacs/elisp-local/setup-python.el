65ed1d34b6147fad627ecaef76e4c9f2bcd1b5ae
Franzelin Fabian (XC-AD/ETV5) <fabian.franzelin@de.bosch.com>
2021-09-17 19:08:06 +0200

    enable ipython3 interpreter and send buffer to python session

--- emacs/elisp-local/setup-python.el
+++ emacs/elisp-local/setup-python.el
@@ -19,11 +19,9 @@
   :ensure-system-package ((pip3 . python3-pip)
                           ("~/.local/lib/python3.6/site-packages/epc" . "python3 -m pip install -U 'epc'")
                           ("~/.local/lib/python3.6/site-packages/ptvsd" . "python3 -m pip install -U 'ptvsd>=4.2'"))
-  :mode (
-         ("\\.py$" . python-mode)
+  :mode (("\\.py$" . python-mode)
          ("SConstruct" . python-mode)
-         ("SConscript" . python-mode)
-         )
+         ("SConscript" . python-mode))
   :hook ((python-mode . (lambda ()
                           (setq company-backends '((company-capf
                                                     company-files)
@@ -35,13 +33,13 @@
                           (require 'dap-python)
                           (lsp))))  ; or lsp-deferred
   :init
+  (setq python-shell-interpreter "ipython3")
+  (setq python-shell-interpreter-args "--simple-prompt -i")
   (setq python-indent-offset 4)
-  (setq python-shell-interpreter "/usr/bin/python3")
   (setq lsp-python-ms-auto-install-server t)
   :config
   ;; delete output buffer on buffer execution
-  (setq py-shell-switch-buffers-on-execute nil)
-  )
+  (setq py-shell-switch-buffers-on-execute nil))
 
 ;; install black and black-macchiato with pip3 install --user -U
 ;; black-macchiato black if black is not available as executable in
@@ -49,8 +47,7 @@
 ;; python3 -m black "${@}"
 (use-package python-black
   :ensure-system-package ((black . "python3 -m pip install --user -U black"))
-  :hook ((python-mode . python-black-on-save-mode))
-  )
+  :hook ((python-mode . python-black-on-save-mode)))
 
 ;; supports virtual environments. To be set with pyvenv-workon
 (use-package pyvenv
