32bad435cb9e8ea9ac8d8ab6669cb9cace566c6b
Franzelin Fabian (XC-AD/ETV5) <fabian.franzelin@de.bosch.com>
2021-10-30 13:30:24 +0200

    add eaf to dotfiles

--- emacs/init.el
+++ emacs/init.el
@@ -797,6 +797,32 @@ FILE: filename"
 (use-package flycheck
   :init (global-flycheck-mode))
 
+;; -------------------------------------------------------------------
+;; Emacs application framework: it does not seem to be stable
+;; -------------------------------------------------------------------
+;; (defvar eaf-load-path (concat emacs-config-home "/site-lisp/emacs-application-framework")
+;;   "Load path for EAF.")
+;; (add-to-list 'load-path eaf-load-path)
+
+;; (use-package eaf
+;;   :load-path eaf-load-path)
+
+;; (use-package eaf-browser
+;;   :ensure nil
+;;   :after eaf
+;;   :custom
+;;   (eaf-browser-continue-where-left-off t)
+;;   (eaf-browser-enable-adblocker t)
+;;   (browse-url-browser-function 'eaf-open-browser)
+;;   (defalias 'browse-web #'eaf-open-browser))
+
+;; (use-package eaf-pdf-viewer
+;;   :ensure nil
+;;   :after eaf
+;;   :custom
+;;   (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
+;;   (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding))
+
 ;; ===================================================================
 ;; Adjusting modes for programming
 ;; ===================================================================
