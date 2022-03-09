70ba7577a55b0f6a565560ea306c47e62491e57b
fabianfranzelin <fabian.franzelin@de.bosch.com>
2022-03-09 13:50:41 +0100

    adds proper history in consult-line call

--- .emacs.d/elisp-local/setup-completion.el
+++ .emacs.d/elisp-local/setup-completion.el
@@ -72,6 +72,9 @@ ARG: position"
 (use-package consult
   :after (projectile vertico)
   :defines consult-buffer-sources
+  ;; Enable automatic preview at point in the *Completions* buffer. This is
+  ;; relevant when you use the default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
   :config
   (setq consult-project-root-function #'dw/get-project-root
         completion-in-region-function #'consult-completion-in-region)
@@ -107,7 +110,7 @@ ARG: position"
          ("M-g g" . consult-goto-line)
          :map minibuffer-local-map
          ("M-y" . yank-pop)
-         ("C-r" . consult-history)))
+         ("C-s" . consult-history)))
 
 (use-package consult-dir
   :custom (consult-dir-project-list-function nil)
