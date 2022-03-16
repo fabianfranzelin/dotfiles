2b723c7572bbcbb41434689231bb5de72f15c7dd
fabianfranzelin <fabian.franzelin@de.bosch.com>
2022-03-16 17:01:41 +0100

    use doom atom theme for treemacs

--- .emacs.d/elisp-local/basic-settings.el
+++ .emacs.d/elisp-local/basic-settings.el
@@ -106,7 +106,7 @@
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   (doom-themes-neotree-config)
   ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-one") ; use "doom-colors" for less minimal icon theme
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
   (doom-themes-treemacs-config)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
