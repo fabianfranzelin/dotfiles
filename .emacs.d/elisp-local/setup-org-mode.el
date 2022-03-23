84c3a7148c1faf5d6bbba0b40f2e9f39a40c2706
fabianfranzelin <fabian.franzelin@de.bosch.com>
2022-03-23 14:52:33 +0100

    setup org roam and use org-modern from minad

--- .emacs.d/elisp-local/setup-org-mode.el
+++ .emacs.d/elisp-local/setup-org-mode.el
@@ -3,97 +3,19 @@
 ;;; Sets up org-mode for Emacs
 
 ;;; Code:
-
-(setq-default org-directory "~/workspace/org"
-		      org-default-notes-file (concat org-directory "/notes.org")
-		      org-agenda-files (directory-files-recursively
-				                org-directory ".org")
-		      org-archive-location (concat org-directory "/archive/"
-						                   (format-time-string "%Y")
-						                   ".org::")
-		      org-return-follows-link t
-		      org-ellipsis "⤵"
-		      org-src-fontify-natively t
-		      org-src-tab-acts-natively t
-		      org-enforce-todo-dependencies t
-		      org-enforce-todo-checkbox-dependencies t
-		      org-agenda-dim-blocked-tasks t)
-
-;; Inspired by this great article:
-;; https://d12frosted.io/posts/2021-01-16-task-management-with-roam-vol5.html
-(defun dw/org-roam-project-files ()
-  "Return a list of note files containing Project tag."
-  (seq-filter
-    #'identity
-    (seq-map
-      #'car
-      (org-roam-db-query
-        [:select file
-        :from tags
-        :where (like tags (quote "%\"Project\"%"))]))))
-
-(defun dw/update-org-agenda-files ()
-  (interactive)
-  (setq org-agenda-files (dw/org-roam-project-files)))
-
-(with-eval-after-load 'org-roam
-  (dw/update-org-agenda-files))
-
-(defun dw/org-path (path)
-  (expand-file-name path org-directory))
-
-(setq org-default-notes-file (dw/org-path "notes.org"))
-
-(defun efs/org-font-setup ()
-  ;; Replace list hyphen with dot
-  (font-lock-add-keywords 'org-mode
-                          '(("^ *\\([-]\\) "
-                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-
-  ;; Set faces for heading levels
-  (dolist (face '((org-level-1 . 1.2)
-                  (org-level-2 . 1.1)
-                  (org-level-3 . 1.05)
-                  (org-level-4 . 1.0)
-                  (org-level-5 . 1.1)
-                  (org-level-6 . 1.1)
-                  (org-level-7 . 1.1)
-                  (org-level-8 . 1.1)))
-    (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
-
-  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
-  (set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
-  (set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
-  (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
-  (set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
-  (set-face-attribute 'line-number nil :inherit 'fixed-pitch)
-  (set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch))
-
-(defun efs/org-mode-setup ()
-  (org-indent-mode)
-  (variable-pitch-mode 1)
-  (visual-line-mode 1))
+;; -------------------------------------------------------------------
+;; Org
+;; -------------------------------------------------------------------
 
 (use-package org
   :after plantuml-mode
   :pin org
-  :ensure-system-package ("/usr/share/fonts/opentype/cantarell" . "sudo apt install fonts-cantarell -y")
   :commands (org-capture org-agenda)
-  :hook ((org-mode . efs/org-mode-setup))
   :config
-  (setq org-ellipsis " ▾")
-
   (setq org-agenda-start-with-log-mode t)
   (setq org-log-done 'time)
   (setq org-log-into-drawer t)
-
-  (setq org-agenda-files (dw/org-path "tasks.org"))
-  (setq org-agenda-files nil)
+  (setq org-babel-python-command "/usr/bin/python3")
 
   (require 'org-habit)
   (add-to-list 'org-modules 'org-habit)
@@ -105,101 +27,6 @@
   (org-babel-do-load-languages 'org-babel-load-languages '((plantuml . t)))
 
   ;; -------------------------------------------------------------------
-  ;; Todos
-  ;; -------------------------------------------------------------------
-  (setq org-todo-keywords
-        '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
-          (sequence "BACKLOG(b)" "READY(r)" "ACTIVE(a)" "|" "COMPLETED(c)" "CANC(k@)")))
-
-
-  ;; Save Org buffers after refiling!
-  (advice-add 'org-refile :after 'org-save-all-org-buffers)
-
-  (setq org-tag-alist
-    '((:startgroup)
-       ; Put mutually exclusive tags here
-       (:endgroup)
-       ("@home" . ?H)
-       ("@work" . ?W)
-       ("agenda" . ?a)
-       ("note" . ?n)))
-
-  ;; -------------------------------------------------------------------
-  ;; Agenda
-  ;; -------------------------------------------------------------------
-  (setq org-agenda-window-setup 'current-window)
-  (setq org-agenda-span 'day)
-  (setq org-agenda-start-with-log-mode t)
-
-  ;; Make done tasks show up in the agenda log
-  (setq org-log-done 'time)
-  (setq org-log-into-drawer t)
-
-  (setq org-columns-default-format "%20CATEGORY(Category) %65ITEM(Task) %TODO %6Effort(Estim){:}  %6CLOCKSUM(Clock) %TAGS")
-
-  ;; Configure custom agenda views
-  (setq org-agenda-custom-commands
-   '(("d" "Dashboard"
-     ((agenda "" ((org-deadline-warning-days 7)))
-      (todo "NEXT"
-        ((org-agenda-overriding-header "Next Tasks")))
-      (tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Projects")))))
-
-    ("n" "Next Tasks"
-     ((todo "NEXT"
-        ((org-agenda-overriding-header "Next Tasks")))))
-
-    ("W" "Work Tasks" tags-todo "+work-email")
-
-    ;; Low-effort next actions
-    ("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
-     ((org-agenda-overriding-header "Low Effort Tasks")
-      (org-agenda-max-todos 20)
-      (org-agenda-files org-agenda-files)))
-
-    ("w" "Workflow Status"
-     ((todo "BACKLOG"
-            ((org-agenda-overriding-header "Project Backlog")
-             (org-agenda-todo-list-sublevels nil)
-             (org-agenda-files org-agenda-files)))
-      (todo "READY"
-            ((org-agenda-overriding-header "Ready for Work")
-             (org-agenda-files org-agenda-files)))
-      (todo "ACTIVE"
-            ((org-agenda-overriding-header "Active Projects")
-             (org-agenda-files org-agenda-files)))
-      (todo "COMPLETED"
-            ((org-agenda-overriding-header "Completed Projects")
-             (org-agenda-files org-agenda-files)))
-      (todo "CANC"
-            ((org-agenda-overriding-header "Cancelled Projects")
-             (org-agenda-files org-agenda-files)))))))
-
-  ;; -------------------------------------------------------------------
-  ;; Capture templates
-  ;; -------------------------------------------------------------------
-  (defun dw/on-org-capture ()
-    ;; Don't show the confirmation header text
-    (setq header-line-format nil)
-
-    ;; Control how some buffers are handled
-    (let ((template (org-capture-get :key t)))
-      (pcase template
-        ("jj" (delete-other-windows)))))
-
-  (add-hook 'org-capture-mode-hook 'dw/on-org-capture)
-
-  (setq org-capture-templates
-    `(("t" "Tasks / Projects")
-      ("tt" "Task" entry (file+olp (dw/org-path "tasks.org") "Inbox")
-           "* TODO %?\n  %U\n  %a\n  %i" :empty-lines 1)))
-
-  (define-key global-map (kbd "C-c j")
-    (lambda () (interactive) (org-capture nil "jj")))
-
-  (efs/org-font-setup)
-
-  ;; -------------------------------------------------------------------
   ;; Clocking
   ;; -------------------------------------------------------------------
   (add-hook 'org-timer-set-hook #'org-clock-in))
@@ -207,22 +34,16 @@
 ;; -------------------------------------------------------------------
 ;; Fonts and Design
 ;; -------------------------------------------------------------------
-(use-package org-bullets
-  :hook (org-mode . org-bullets-mode)
-  :custom
-  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
-
 ;; For details, see enables https://github.com/abo-abo/org-download
 (use-package org-download)
 
 (with-eval-after-load 'org
   (org-babel-do-load-languages
-      'org-babel-load-languages
-      '((emacs-lisp . t)
-      (python . t)))
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (python . t)))
 
   (push '("conf-unix" . conf-unix) org-src-lang-modes))
-
 
 (with-eval-after-load 'org
   ;; This is needed as of Org 9.2
@@ -233,22 +54,41 @@
   (add-to-list 'org-structure-template-alist '("py" . "src python"))
   (add-to-list 'org-structure-template-alist '("cc" . "src c++")))
 
-
-;; Automatically tangle our Emacs.org config file when we save it
-(defun efs/org-babel-tangle-config ()
-  (when (string-equal (file-name-directory (buffer-file-name))
-                      (expand-file-name user-emacs-directory))
-    ;; Dynamic scoping to the rescue
-    (let ((org-confirm-babel-evaluate nil))
-      (org-babel-tangle))))
-
-(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
-
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((emacs-lisp . t)
    (python . t)
    (shell . t)))
+
+;; -------------------------------------------------------------------
+;; Org modern
+;; -------------------------------------------------------------------
+
+(setq
+ ;; Edit settings
+ org-auto-align-tags nil
+ org-tags-column 0
+ org-catch-invisible-edits 'show-and-error
+ org-special-ctrl-a/e t
+ org-insert-heading-respect-content t
+
+ ;; Org styling, hide markup etc.
+ org-hide-emphasis-markers t
+ org-pretty-entities t
+ org-ellipsis "…"
+
+ ;; Agenda styling
+ org-agenda-block-separator ?─
+ org-agenda-time-grid
+ '((daily today require-timed)
+   (800 1000 1200 1400 1600 1800 2000)
+   " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
+ org-agenda-current-time-string
+ "⭠ now ─────────────────────────────────────────────────")
+
+;; Enable org-modern-mode
+(add-hook 'org-mode-hook #'org-modern-mode)
+(add-hook 'org-agenda-finalize-hook #'org-modern-agenda)
 
 (provide 'setup-org-mode)
 
