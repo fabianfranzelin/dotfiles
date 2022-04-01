0accd0630ceb7b56a409211eff6fcc7ecd45e4cd
fabianfranzelin <fabian.franzelin@de.bosch.com>
2022-04-01 16:52:19 +0200

    add mapping for compile commands

--- .emacs.d/elisp-local/setup-cc.el
+++ .emacs.d/elisp-local/setup-cc.el
@@ -12,6 +12,34 @@
 ;; -----------------------------------------------------------------------------------
 ;; Formatting and linting
 
+
+;; Get to work with Docker mounted workspaces: Usually, the docker
+;; container I am running does not contain a
+
+(defvar +ccls-container-workspace "/workspace"
+  "Location where the workspace is mounted in the container.")
+
+(defun ff/search-replace (file-path regex-str replace-str)
+  "Replace content in file.
+FILE-PATH: file to be changed
+REGEX-STR: regular expression to be replaced
+REPLACE-STR: string that replaces all regex matches"
+  (interactive "P")
+  (with-temp-file file-path
+    (insert-file-contents file-path)
+    (goto-char (point-min))
+    (while (re-search-forward regex-str nil t)
+      (replace-match replace-str))))
+
+
+(defun ff/container-host-compile-commands-mapping ()
+  "Adjusts the compile commands json of CMake to match the host
+system and not the containers."
+  (interactive)
+  (ff/search-replace (concat (plist-get ccls-initialization-options :compilationDatabaseDirectory) "/compile_commands.json")
+                     +ccls-container-workspace
+                     (projectile-project-root)))
+
 (defun ff/clang-format-buffer-smart ()
   "Reformat buffer if .clang-format exists in the projectile root."
   (interactive)
@@ -207,7 +235,9 @@
       (font-lock-add-keywords
        nil '(("\\<[A-Z]*_[0-9A-Z_]+\\>" . font-lock-constant-face)
              ("\\<[A-Z]\\{3,\\}\\>"  . font-lock-constant-face))
-       t))))
+       t)))
+  :bind (;; TODO this is a hack for now. It should only be enabled in C++ mode
+         ("C-c p r" . ff/container-host-compile-commands-mapping)))
 
 ;; Major mode for editing QT Declarative (QML) code.
 ;; https://github.com/coldnew/qml-mode
