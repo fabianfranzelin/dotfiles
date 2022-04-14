02ece5d65f0077fdecd23928fcb56063d983bb26
Franzelin Fabian (XC-AD/ETV5) <fabian.franzelin@de.bosch.com>
2022-04-14 14:10:21 +0200

    do not report native comp warnings or errors

--- .emacs.d/elisp-local/basic-settings.el
+++ .emacs.d/elisp-local/basic-settings.el
@@ -92,6 +92,9 @@
 ;; Iterate through CamelCase
 (global-subword-mode t)
 
+;; do not report warnings or errors of native comp
+(setq native-comp-async-report-warnings-errors nil)
+
 ;; Color theme
 (use-package doom-themes
   :ensure t
