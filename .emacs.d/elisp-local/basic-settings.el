40750a7f51ded1f113f5bfa832243945db8d8ac7
fabianfranzelin <fabian.franzelin@de.bosch.com>
2022-04-28 13:50:15 +0200

    set cache variable for persp state

--- .emacs.d/elisp-local/basic-settings.el
+++ .emacs.d/elisp-local/basic-settings.el
@@ -235,7 +235,7 @@
 (use-package perspective
   :custom
   (persp-mode-prefix-key (kbd "C-x x"))
-  (persp-state-default-file (concat emacs-config-home "/.cache/persp-state.el"))
+  (persp-state-default-file (concat user-emacs-directory "/persp-state.el"))
   :init
   (unless (equal persp-mode t)
     (persp-mode))
