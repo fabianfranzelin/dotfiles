dfae53417ff70d0d4cca1746693a08cbfa0050d7
Franzelin Fabian (XC-DX/ESB4) <fabian.franzelin@de.bosch.com>
2022-07-03 19:23:31 +0200

    add Davids org present configuration

--- .emacs.d/elisp-local/setup-org-mode.el
+++ .emacs.d/elisp-local/setup-org-mode.el
@@ -89,6 +89,56 @@
   :hook ((org-mode . org-modern-mode)
          (org-agenda-finalize . org-modern-agenda)))
 
+;; -------------------------------------------------------------------
+;; org-present
+;; -------------------------------------------------------------------
+
+(defun dw/org-present-prepare-slide ()
+  (org-overview)
+  (org-show-entry)
+  (org-show-children))
+
+(defun dw/org-present-hook ()
+  (setq-local face-remapping-alist '((default (:height 1.5) variable-pitch)
+                                     (header-line (:height 4.5) variable-pitch)
+                                     (org-document-title (:height 1.75) org-document-title)
+                                     (org-code (:height 1.55) org-code)
+                                     (org-verbatim (:height 1.55) org-verbatim)
+                                     (org-block (:height 1.25) org-block)
+                                     (org-block-begin-line (:height 0.7) org-block)))
+  (setq header-line-format " ")
+  (org-appear-mode -1)
+  (org-display-inline-images)
+  (dw/org-present-prepare-slide))
+
+(defun dw/org-present-quit-hook ()
+  (setq-local face-remapping-alist '((default variable-pitch default)))
+  (setq header-line-format nil)
+  (org-present-small)
+  (org-remove-inline-images)
+  (org-appear-mode 1))
+
+(defun dw/org-present-prev ()
+  (interactive)
+  (org-present-prev)
+  (dw/org-present-prepare-slide))
+
+(defun dw/org-present-next ()
+  (interactive)
+  (org-present-next)
+  (dw/org-present-prepare-slide))
+
+(use-package org-appear)
+
+(use-package org-present
+  :after org-appear
+  :hook ((org-present-mode . dw/org-present-hook)
+         (org-present-mode . visual-line-mode)
+         (org-present-mode-quit . dw/org-present-quit-hook))
+  :bind (:map org-present-mode-keymap
+              ("C-c C-j" . dw/org-present-next)
+              ("C-c C-k" . dw/org-present-prev)))
+
 (provide 'setup-org-mode)
 
 ;;; setup-org-mode.el ends here
