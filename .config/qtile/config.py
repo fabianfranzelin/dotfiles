01d8eec72c74f8c1882afb1c2593d5cb0398d757
fabianfranzelin <fabian.franzelin@de.bosch.com>
2022-09-13 12:06:26 +0200

    add evolution alarm notify as floating window

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -273,6 +273,7 @@ floating_layout = layout.Floating(
         Match(wm_class="makebranch"),  # gitk
         Match(wm_class="maketag"),  # gitk
         Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(wm_class="evolution-alarm-notify"),  # evolution notifier
         Match(title="branchdialog"),  # gitk
         Match(title="pinentry"),  # GPG key password entry
         Match(wm_class="r_x11"),  # plots with R
