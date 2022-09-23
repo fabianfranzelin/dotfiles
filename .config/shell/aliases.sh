f47fa5f94cf52339602cd82b19c0341cce84374f
fabianfranzelin <fabian.franzelin@de.bosch.com>
2022-09-23 15:16:47 +0200

    fix of is linux thing

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -2,7 +2,7 @@
 
 # shellcheck disable=SC1090,SC2139
 
-if ( is_linux ) then
+if ( __is_linux ) then
    # these aliases require systemd running, hence it just works on
    # native Linux
    alias reboot='systemctl reboot'
