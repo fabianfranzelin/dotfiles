a5a04d2e8afa54fc6a70dcadc887dba6986d754a
fabianfranzelin <fabian.franzelin@de.bosch.com>
2022-09-28 14:39:46 +0200

    add proxy setting for st39

--- .config/shell/workstations/st39-c-00066.sh
+++ .config/shell/workstations/st39-c-00066.sh
@@ -12,6 +12,18 @@ export __SHELL_LIB="${XDG_CONFIG_HOME}/shell"
 [ -f "${__SHELL_LIB}/workstations/b_credentials.sh" ] && . "${__SHELL_LIB}/workstations/b_credentials.sh"
 
 #------------------------------------------------------------------------------#
+# proxy
+
+export no_proxy="localhost,127.0.0.*,192.168.*,.local,172.*,.bosch.*,.bosch-iot-cloud.com"
+export http_proxy="http://127.0.0.1:3128"
+export https_proxy="http://127.0.0.1:3128"
+export HTTP_PROXY="http://127.0.0.1:3128"
+export HTTPS_PROXY="http://127.0.0.1:3128"
+export ALL_proxy="http://127.0.0.1:3128"
+export all_proxy="http://127.0.0.1:3128"
+export ftp_proxy="http://127.0.0.1:3128"
+
+#------------------------------------------------------------------------------#
 # AOS
 export AOS_BASE_HOME="${HOME}/workspace/aos_base"
 export AOS_BUILD_DIR="${AOS_BASE_HOME}/build"
