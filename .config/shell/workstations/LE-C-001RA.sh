a664b07df89d5682f173e6d28f43f8e0e255e1fe
Fabian Franzelin (XC-DX/ESB4) <fabian.franzelin@de.bosch.com>
2022-10-18 09:51:38 +0200

    add new workstation

---
+++ .config/shell/workstations/LE-C-001RA.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+
+# shellcheck disable=SC1090
+
+# Load specific settings for WSL
+
+#------------------------------------------------------------------------------#
+# load credentials
+export __SHELL_LIB="${XDG_CONFIG_HOME}/shell"
+[ -f "${__SHELL_LIB}/workstations/b_credentials.sh" ] && . "${__SHELL_LIB}/workstations/b_credentials.sh"
+
+#------------------------------------------------------------------------------#
+# WSL specific stuff
+
+# .profile
+export no_proxy="localhost,127.0.0.*,192.168.*,.local,172.*,.bosch.*,.bosch-iot-cloud.com"
+export http_proxy="http://127.0.0.1:3128"
+export https_proxy="http://127.0.0.1:3128"
+export HTTP_PROXY="http://127.0.0.1:3128"
+export HTTPS_PROXY="http://127.0.0.1:3128"
+export ALL_proxy="http://127.0.0.1:3128"
+export all_proxy="http://127.0.0.1:3128"
+export ftp_proxy="http://127.0.0.1:3128"
+
+# .bashrc
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+alias init='wsl.exe -d wsl-vpnkit service wsl-vpnkit start; sudo service cntlm start; sudo service docker start'
+
+#------------------------------------------------------------------------------#
+# Azure DevOps
+# Run cat BOSCH-CA-DE_pem.cer /opt/az/lib/python3.6/site-packages/certifi/cacert.pem > azure-bosch-cert.pem
+export REQUESTS_CA_BUNDLE="${HOME}/.local/share/certificates/bosch/azure-bosch-cert.pem"
+
+#------------------------------------------------------------------------------#
+# Ford
+if [ -f "${HOME}/.forddat3/devcontainer" ]; then
+    . "${HOME}/.forddat3/devcontainer"
+fi
