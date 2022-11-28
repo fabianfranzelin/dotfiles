374ebb83f9a2b95658155b280eb0e461a5c063f1
fabianfranzelin <fabian.franzelin@de.bosch.com>
2022-11-28 15:04:21 +0100

    move sensitive information

--- .config/shell/workstations/st39-c-00066.sh
+++ .config/shell/workstations/st39-c-00066.sh
@@ -5,15 +5,11 @@
 # Load specific settings for Bosch
 
 #------------------------------------------------------------------------------#
-# load credentials
-# ----------------------------------------------------
 # Dotfiles setup
 export __SHELL_LIB="${XDG_CONFIG_HOME}/shell"
-[ -f "${__SHELL_LIB}/workstations/b_credentials.sh" ] && . "${__SHELL_LIB}/workstations/b_credentials.sh"
 
 #------------------------------------------------------------------------------#
 # proxy
-
 
 export no_proxy="localhost,127.0.0.1,127.*,::1,172.16.*,172.17.*,172.18.*,172.19.*,172.20.*,172.21.*,172.22.*,172.23.*,172.24.*,172.25.*,172.26.*,172.27.*,172.28.*,172.29.*,172.30.*,172.31.*,192.168.*,10.*,de.bosch.com,apac.bosch.com,emea.bosch.com,us.bosch.com,rb-artifactory.bosch.com,sourcecode01.de.bosch.com,sourcecode.socialcoding.bosch.com,dev.bosch.com"
 export http_proxy="http://127.0.0.1:3128"
