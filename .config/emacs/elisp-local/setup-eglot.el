2e152766de9010671036db6cfc4fa40902cea990
Franzelin Fabian (XC-DX/ESB4) <fabian.franzelin@de.bosch.com>
2022-12-13 23:11:15 +0100

    adds some default configs for lsp servers

--- .config/emacs/elisp-local/setup-eglot.el
+++ .config/emacs/elisp-local/setup-eglot.el
@@ -20,26 +20,23 @@
   :config
   (setq read-process-output-max (* 1024 1024))
   (setq eglot-events-buffer-size 10)
+
+  ;; Python
+  (add-to-list 'eglot-server-programs
+               `(python-mode "python3" "-m" "pylsp"))
+
   ;; C++
   (add-to-list 'eglot-server-programs
                `((c++-mode c-mode) "clangd-12"))
+
   ;; Sphinx, rst-mode
   (defclass eglot-esbonio (eglot-lsp-server) ()
     :documentation "Esbonio Language Server.")
-
-  (cl-defmethod eglot-initialization-options ((server eglot-esbonio))
-    "Passes the initializationOptions required to run the server."
-    `(:sphinx (:confDir "${workspaceRoot}"
-                        :srcDir "${confDir}")
-              :server (:logLevel "debug")))
-
   (add-to-list 'eglot-server-programs
                `(rst-mode . (eglot-esbonio
                              ,(executable-find "python3")
                              "-m" "esbonio")))
 
-  ;; disable flymake activation
-  ;; (add-to-list 'eglot-stay-out-of 'flymake)
   :bind (("C-c l w s" . eglot)
          ("C-c l w r" . eglot-reconnect)
          ("C-c l w k" . eglot-shutdown)
@@ -48,25 +45,8 @@
          ("C-c l i" . eglot-code-action-organize-imports)
          ("C-c l r" . eglot-rename)
          ("C-c l d" . flymake-show-buffer-diagnostics)
-         ("C-h ." . eldoc)))
-
-;; (use-package flymake-collection
-;;   :hook ((c++-mode . flymake-mode)
-;;          (c-mode . flymake-mode)
-;;          (java-mode . flymake-mode)
-;;          (python-mode . flymake-mode)
-;;          (typescript-mode . flymake-mode)
-;;          (json-mode . flymake-mode)
-;;          (yaml-mode . flymake-mode)
-;;          (sh-mode . flymake-mode)
-;;          (cmake-mode . flymake-mode)
-;;          (dockerfile-mode . flymake-mode))
-;;   :init
-;;   (setcdr (assoc `python-mode flymake-collection-config)
-;;           '(flymake-collection-pylint
-;;             flymake-collection-mypy
-;;             (flymake-collection-pycodestyle :disabled t)
-;;             (flymake-collection-flake8 :disabled t))))
+         ("C-h ." . eldoc)
+         ("C-c l e" . eglot-stderr-buffer)))
 
 (use-package realgud
   :after (org)
