37ebc21a827a5b89b60626b977a6c0797fad3439
Franzelin Fabian (XC-DX/ESB4) <fabian.franzelin@de.bosch.com>
2023-01-23 09:14:26 +0100

    move pyvenv call to python context

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,8 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((eval . (pyvenv-deactivate))))
- (python-mode . ((flycheck-add-next-checker . ('python-pylint 'python-mypy))
+((python-mode . ((eval . (pyvenv-deactivate))
+                 (flycheck-add-next-checker . ('python-pylint 'python-mypy))
                  (eval . (add-to-list 'flycheck-disabled-checkers 'python-flake8))
                  (eglot-workspace-configuration
                   . (:pylsp (:plugins (:jedi_completion (:include_params t
