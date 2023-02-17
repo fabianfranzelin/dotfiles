fc9e9dfaa6215993df88aebd98d0ac5cdc8b8ab1
fabianfranzelin <fabian.franzelin@de.bosch.com>
2023-02-17 12:06:04 +0100

    change project markers and update cc-example project

--- examples/cc-project/.dir-locals.el
+++ examples/cc-project/.dir-locals.el
@@ -1,7 +1,7 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((compile-command . "cd ./examples/cc-project/build && make")))
+((nil . ((compile-command . "cd ./build && make")))
  (c++-mode . ((eval . (setq-local ff/cc-build-folder-container nil))
               (eval . (setq-local ff/cc-conan-cache-container nil))
               (eval . (setq-local ff/cc-conan-cache-host nil)))))
