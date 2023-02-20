4a191542b702a00c82ca3469d6edfe7a97df95ff
fabianfranzelin <fabian.franzelin@de.bosch.com>
2023-02-20 09:57:16 +0100

    enable async native compile again

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -41,7 +41,9 @@
   (add-to-list 'native-comp-eln-load-path (expand-file-name "var/eln-cache/" user-emacs-directory))
 
   ;; Silence compiler warnings as they can be pretty disruptive
-  (setq native-comp-async-report-warnings-errors nil))
+  (setq native-comp-async-report-warnings-errors nil)
+  ;; Make native compilation happens asynchronously
+  (setq native-comp-deferred-compilation t))
 
 ;; -------------------------------------------------------------------
 ;;; UI configuration
