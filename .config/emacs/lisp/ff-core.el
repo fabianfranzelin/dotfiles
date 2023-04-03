f8ed7e6fc6877eebc451f9b4031adbfae6508c2c
Fabian Franzelin <fabian.franzelin@gmail.com>
2023-04-03 10:22:22 +0200

    update gpg and pass configs

--- .config/emacs/lisp/ff-core.el
+++ .config/emacs/lisp/ff-core.el
@@ -457,7 +457,8 @@ PROJECT-ROOT: Path to the root directory of the current project."
 (require 'auth-source)
 (add-to-list 'auth-sources '(password-store))
 
-(use-package pass)
+(use-package pass
+  :custom ((pass-show-keybindings nil)))
 
 ;; -------------------------------------------------------------------
 ;; Open files externally
