1e9f2acb2da8f4366a48b0d870fb8dc6db002c4c
Fabian Franzelin <fabian.franzelin@gmail.com>
2023-04-07 10:07:26 +0200

    add gptel

---
+++ .config/emacs/lisp/ff-ai-assistant.el
@@ -0,0 +1,12 @@
+;;; ff-ai-assistant.el --- Setup AI assistant
+
+;;; Commentary:
+;; Setup Chagpt as AI assistant
+
+;;; Code:
+
+(use-package gptel)
+
+(provide 'ff-ai-assistant)
+
+;;; ff-ai-assistant.el ends here
