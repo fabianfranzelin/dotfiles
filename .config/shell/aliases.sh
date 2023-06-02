271c16b5a181f55dd4c01fbcb05027dc5115f9a9
Franzelin Fabian (XC-DX/ESB4) <fabian.franzelin@de.bosch.com>
2023-06-02 12:45:42 +0200

    remove publickey authentication for ws

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -83,3 +83,6 @@ alias ff='find_file'
 
 # Set keyboard layout to German and disable Caps-Lock
 alias set_default_keyboard_layout='setxkbmap -layout de -option ctrl:nocaps'
+
+# SSH logins
+alias ssh_ws='sshpass -p "$(pass passwords/$(pass usernames/bosch)@login)" ssh ws'
