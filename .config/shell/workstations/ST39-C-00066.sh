c7a95b51e53a7719d7a860f634635b48118bfc81
fabianfranzelin <fabian.franzelin@de.bosch.com>
2023-06-05 11:59:23 +0200

    remove proxy setup for ws

--- .config/shell/workstations/ST39-C-00066.sh
+++ .config/shell/workstations/ST39-C-00066.sh
@@ -13,15 +13,6 @@ export __SHELL_LIB="${XDG_CONFIG_HOME}/shell"
 
 pass passwords/$(pass usernames/bosch)@login | kinit > /dev/null
 
-export no_proxy="localhost,127.0.0.1,127.*,::1,172.16.*,172.17.*,172.18.*,172.19.*,172.20.*,172.21.*,172.22.*,172.23.*,172.24.*,172.25.*,172.26.*,172.27.*,172.28.*,172.29.*,172.30.*,172.31.*,192.168.*,10.*,de.bosch.com,apac.bosch.com,emea.bosch.com,us.bosch.com,rb-artifactory.bosch.com,sourcecode01.de.bosch.com,sourcecode.socialcoding.bosch.com,dev.bosch.com"
-export http_proxy="http://127.0.0.1:3128"
-export https_proxy="http://127.0.0.1:3128"
-export HTTP_PROXY="http://127.0.0.1:3128"
-export HTTPS_PROXY="http://127.0.0.1:3128"
-export ALL_proxy="http://127.0.0.1:3128"
-export all_proxy="http://127.0.0.1:3128"
-export ftp_proxy="http://127.0.0.1:3128"
-
 #------------------------------------------------------------------------------#
 # AOS
 export AOS_BASE_HOME="${HOME}/workspace/aos_base"
