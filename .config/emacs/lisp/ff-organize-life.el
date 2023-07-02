6048a153b6703667813c43ed5c3d07fc530cfb1a
fabianfranzelin <fabian.franzelin@de.bosch.com>
2023-07-02 21:52:30 +0200

    add some more useful templates and shortcuts

--- .config/emacs/lisp/ff-organize-life.el
+++ .config/emacs/lisp/ff-organize-life.el
@@ -137,28 +137,30 @@
   (org-roam-database-connector 'sqlite-builtin)
   (org-roam-completion-everywhere t)
   (org-roam-db-gc-threshold most-positive-fixnum)
-  ;; (org-roam-capture-templates
-  ;;  '(("d" "default" plain "%?"
-  ;;     :target (file+head "%<%Y-%m-%d-%H-%M-%S>-${slug}.org" "#+title: ${title}\n")
-  ;;     :unnarrowed t)
-  ;;    ("lt" "link task" entry
-  ;;     "* TODO %^{Link}"
-  ;;     :target (file+olp "inbox.org" ("tasks"))
-  ;;     :unnarrowed t
-  ;;     :immediate-finish)))
+  (org-roam-capture-templates
+   '(("d" "default" plain
+      "%?"
+      :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                         "#+title: ${title}\n")
+      :unnarrowed t)))
   ;; org-roam-dailies
   (org-roam-dailies-directory (expand-file-name "journal" org-directory))
-  ;; (org-roam-dailies-capture-templates
-  ;;  '(("d" "default" entry "* %?"
-  ;;     :target (file+head "%<%Y-%m-%d-%H-%M-%S>.org" "#+title: ${title}\n")
-  ;;     :unnarrowed t)
-  ;;    ("t" "Task" entry
-  ;;     "* TODO %?\n  %U\n  %a\n  %i"
-  ;;     :target (file+head+olp "journal/%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d %a>\n\n[[roam:%<%Y-%B>]]\n\n" ("tasks"))
-  ;;     :empty-lines 1)
-  ;;    ("m" "meeting" entry
-  ;;     "* %<%I:%M %p> - %^{Meeting Title}  :meetings:\n\n%?\n\n"
-  ;;     :target (file+head+olp "journal/%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d %a>\n\n[[roam:%<%Y-%B>]]\n\n" ("log"))))))
+  (org-roam-dailies-capture-templates
+   '(("d" "default" plain
+      "* %?"
+      :target (file+head "%<%Y%m%d%H%M>.org" "#+title: %<%Y-%m-%d-%H-%M>\n")
+      :unarrowed t)
+     ("t" "task" entry
+      "* TODO %^{Todo title}\n %U\n %a\n %i\n %?"
+      :if-new (file+head "%<%Y%m%d%H%M>-todo.org"
+                         "#+title: %<%Y-%m-%d %a>\n\n")
+      :empty-lines 1
+      :unarrowed t)
+     ("m" "meeting" entry
+      "* %<%I:%M %p> - %^{Meeting Title}  :meetings:\n\n%?\n\n"
+      :if-new (file+head "%<%Y%m%d%H%M>-meeting.org"
+                         "#+title: %<%Y-%m-%d %a>\n\n")
+      :unarrowed t)))
   :init
   (org-roam-db-autosync-mode t)
   :config
@@ -169,14 +171,11 @@
                  (direction . right)
                  (window-width . 0.33)
                  (window-height . fit-window-to-buffer)))
-  :bind (("C-x n f"  . org-roam-node-find)
+  :bind (("C-x n c" . org-roam-capture)
+         ("C-x n f"  . org-roam-node-find)
          ("C-x n b" . org-roam-buffer-toggle)
-         ("C-x n d"  . org-roam-dailies-find-date)
-         ("C-x n c"  . org-roam-dailies-capture-today)
-         ("C-x n C r" . org-roam-dailies-capture-tomorrow)
-         ("C-x n t"  . org-roam-dailies-goto-today)
-         ("C-x n y"  . org-roam-dailies-goto-yesterday)
-         ("C-x n r"  . org-roam-dailies-goto-tomorrow)
+         ("C-x n n"  . org-roam-dailies-capture-today)
+         ("C-x n d"  . org-roam-dailies-goto-today)
          ("C-x n g"  . org-roam-graph)))
 
 (with-eval-after-load 'org-roam
