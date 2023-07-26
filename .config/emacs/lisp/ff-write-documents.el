48e4df68e287bd0c7c3cb5db091cb0132cffe1f8
Franzelin Fabian (XC-DX/ESB4) <fabian.franzelin@de.bosch.com>
2023-07-26 18:07:54 +0200

    update plantuml and languagetool version

--- .config/emacs/lisp/ff-write-documents.el
+++ .config/emacs/lisp/ff-write-documents.el
@@ -112,7 +112,7 @@ JAR-PATH: expected binary file in the extracted folder."
   (ff/ensure-apt-package "graphviz" "dot")
 
   ;; Consider using (plantuml-download-jar) as alternative
-  (defvar plantuml-version "1.2023.4"
+  (defvar plantuml-version "1.2023.10"
     "Version number of plantuml binary")
   (defvar plantuml-name (concat "plantuml-" plantuml-version ".jar")
     "Name of plantuml executable")
