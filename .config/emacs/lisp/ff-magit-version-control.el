ab537245dae2d17858aae17efbac75deb0b4f682
fabianfranzelin <fabian.franzelin@de.bosch.com>
2023-08-30 16:00:46 +0200

    remove vdiff and start using ediff

--- .config/emacs/lisp/ff-magit-version-control.el
+++ .config/emacs/lisp/ff-magit-version-control.el
@@ -12,40 +12,11 @@
 
 (use-package magit-lfs)
 
-;; -------------------------------------------------------------------
-;; Vdiff: Vim like diff
-;; -------------------------------------------------------------------
-(use-package vdiff
-  :bind (("C-c v f" . vdiff-files)
-         ("C-c v b" . vdiff-buffers)
-         ("C-c v q" . vdiff-quit)
-         ("C-c v h" . vdiff-hydra/body)))
+;; configure ediff
 
-(use-package vdiff-magit
-  :after (magit vdiff)
-  :config
-  (define-key magit-mode-map "e" 'vdiff-magit-dwim)
-  (define-key magit-mode-map "E" 'vdiff-magit)
-  (transient-suffix-put 'magit-dispatch "e" :description "vdiff (dwim)")
-  (transient-suffix-put 'magit-dispatch "e" :command 'vdiff-magit-dwim)
-  (transient-suffix-put 'magit-dispatch "E" :description "vdiff")
-  (transient-suffix-put 'magit-dispatch "E" :command 'vdiff-magit)
-
-  ;; This flag will default to using ediff for merges.
-  ;; (setq vdiff-magit-use-ediff-for-merges nil)
-
-  ;; Whether vdiff-magit-dwim runs show variants on hunks.  If non-nil,
-  ;; vdiff-magit-show-staged or vdiff-magit-show-unstaged are called based on what
-  ;; section the hunk is in.  Otherwise, vdiff-magit-dwim runs vdiff-magit-stage
-  ;; when point is on an uncommitted hunk.
-  ;; (setq vdiff-magit-dwim-show-on-hunks nil)
-
-  ;; Whether vdiff-magit-show-stash shows the state of the index.
-  ;; (setq vdiff-magit-show-stash-with-index t)
-
-  ;; Only use two buffers (working file and index) for vdiff-magit-stage
-  ;; (setq vdiff-magit-stage-is-2way nil)
-  )
+;; This is what you probably want if you are using a tiling window
+;; manager under X, such as ratpoison.
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
 
 (provide 'ff-magit-version-control)
 
