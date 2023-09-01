d6ecae6639a1e640a36c53295f8001c664de3c8b
fabianfranzelin <fabian.franzelin@de.bosch.com>
2023-09-01 14:48:07 +0200

    restore window configuration when leaving ediff

--- .config/emacs/lisp/ff-magit-version-control.el
+++ .config/emacs/lisp/ff-magit-version-control.el
@@ -18,6 +18,22 @@
 ;; manager under X, such as ratpoison.
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
 
+;; save and restore window configuration when entering and quitting
+;; ediff
+;; https://www.emacswiki.org/emacs/EdiffMode
+(add-hook 'ediff-load-hook
+          (lambda ()
+
+            (add-hook 'ediff-before-setup-hook
+                      (lambda ()
+                        (setq ediff-saved-window-configuration (current-window-configuration))))
+
+            (let ((restore-window-configuration
+                   (lambda ()
+                     (set-window-configuration ediff-saved-window-configuration))))
+              (add-hook 'ediff-quit-hook restore-window-configuration 'append)
+              (add-hook 'ediff-suspend-hook restore-window-configuration 'append))))
+
 (provide 'ff-magit-version-control)
 
 ;;; ff-magit-version-control.el ends here
