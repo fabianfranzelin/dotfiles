0166c6439dd51b76855264334d83d2ac43b66e38
Franzelin Fabian (XC-DX/ESB4) <fabian.franzelin@de.bosch.com>
2023-10-09 11:25:20 +0200

    some stuff

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -82,7 +82,7 @@ alias emacs_restart="edr"
 alias ff='find_file'
 
 # Set keyboard layout to German and disable Caps-Lock
-alias set_default_keyboard_layout='setxkbmap -layout de -option ctrl:nocaps && xmodmap -e "keycode 111 = Alt_L Meta_L Alt_L Meta_L"'
+alias set_default_keyboard_layout='setxkbmap -layout de -option ctrl:nocaps'
 
 # SSH logins
 alias ssh_ws='sshpass -p "$(pass passwords/$(pass usernames/bosch)@nt)" ssh ws'
