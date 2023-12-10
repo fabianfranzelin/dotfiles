97ae39164574b6e937caaa356c086f64942241a9
Fabian Franzelin <fabian.franzelin@gmail.com>
2023-12-10 18:57:18 +0100

    update some stuff

--- .config/minimal-emacs/init.el
+++ .config/minimal-emacs/init.el
@@ -12,6 +12,32 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
+(cl-defun ff/vc-install (&key (fetcher "github") repo name rev backend)
+  "Install a package from a remote if it's not already installed.
+This is a thin wrapper around `package-vc-install' in order to
+make non-interactive usage more ergonomic.  Takes the following
+named arguments:
+
+- FETCHER the remote where to get the package (e.g., \"gitlab\").
+  If omitted, this defaults to \"github\".
+
+- REPO should be the name of the repository (e.g.,
+  \"slotThe/arXiv-citation\".
+
+- NAME, REV, and BACKEND are as in `package-vc-install' (which
+  see)."
+  (let* ((url (format "https://www.%s.com/%s" fetcher repo))
+         (iname (when name (intern name)))
+         (pac-name (or iname (intern (file-name-base repo)))))
+    (cond ((and name
+                (not (package-installed-p name)))
+           (package-vc-install name))
+          ((and repo
+                (not (package-installed-p pac-name)))
+           (package-vc-install url iname rev backend))
+          (t
+           (message "Do not do anything for %s" name)))))
+
 ;; -------------------------------------------------------------------
 ;; Before we do anything, set up the no littering package
 (package-install 'no-littering)
