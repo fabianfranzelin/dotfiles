1c41c27f83fedd4cbfbeb42de17cc0744536ca4c
Franzelin Fabian (XC-DX/ESB4) <fabian.franzelin@de.bosch.com>
2023-12-21 13:24:01 +0100

    fix copilot warning when looking up Emacs lisp indentation offset

--- .config/emacs/lisp/ff-programming-settings.el
+++ .config/emacs/lisp/ff-programming-settings.el
@@ -210,6 +210,9 @@ FORCE: force update of grammars"
                                                     (other-window 1)
                                                     (ielm)))
 
+;; set default indentation to 2 spaces
+(customize-set-variable 'lisp-indent-offset nil)
+
 ;; -------------------------------------------------------------------
 ;; C/C++
 ;; -------------------------------------------------------------------
