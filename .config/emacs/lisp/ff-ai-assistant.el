888e19ef3d961d76d267ba1b616965d1dbe59ac4
Franzelin Fabian (XC-DX/ESB4) <fabian.franzelin@de.bosch.com>
2024-01-31 22:33:51 +0100

    update gptel hooks

--- .config/emacs/lisp/ff-ai-assistant.el
+++ .config/emacs/lisp/ff-ai-assistant.el
@@ -18,10 +18,10 @@
   (gptel-default-mode 'org-mode)
   :hook
   ;; move cursor to next heading when response is posted
-  (gptel-post-response . (lambda ()
-                           (when (derived-mode-p 'org-mode)
-                             (org-forward-element)
-                             (org-end-of-line))))
+  (gptel-post-response-functions . (lambda ()
+                                     (when (derived-mode-p 'org-mode)
+                                       (org-forward-element)
+                                       (org-end-of-line))))
   :bind (("C-c g" . gptel)))
 
 ;; GitHub copilot
