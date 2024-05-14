775e5fa256b383c9d6cbdb9477b07d10ae770828
Fabian Franzelin <fabian.franzelin@gmail.com>
2024-05-14 22:41:35 +0200

    add a first configuration for nyxt

---
+++ .config/nyxt/config.lisp
@@ -0,0 +1,14 @@
+;; emacs keybindings
+(define-configuration buffer
+  ((default-modes
+    (pushnew 'nyxt/mode/emacs:emacs-mode %slot-value%))))
+
+;; Adblocking
+(define-configuration web-buffer
+  ((default-modes
+    (pushnew 'nyxt/mode/blocker:blocker-mode %slot-value%))))
+
+;; reduce tracking mode
+(define-configuration web-buffer
+  ((default-modes
+    (pushnew 'nyxt/mode/reduce-tracking:reduce-tracking-mode %slot-value%))))
