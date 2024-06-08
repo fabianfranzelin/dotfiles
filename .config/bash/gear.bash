3424b5bcb29e14a83765ed82f108ab6ccffbb962
Fabian Franzelin <fabian.franzelin@gmail.com>
2024-06-08 10:53:43 +0200

    update some stuff

--- .config/bash/gear.bash
+++ .config/bash/gear.bash
@@ -13,6 +13,7 @@ for __DIR in "${__DIRS[@]}"; do
         # is folder empty?
         if [ -n "$(ls -A "${__DIR}/func")" ]; then
             for __FILE in "${__DIR}/func/"*; do
+		echo "Source ${__FILE}"
                 source "${__FILE}";
             done
         fi
