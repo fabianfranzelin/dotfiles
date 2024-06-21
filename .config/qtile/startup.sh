c99786d96a66c30728724e49bebf46b565f4747f
Franzelin Fabian (XC-DX/ESB4) <fabian.franzelin@de.bosch.com>
2024-06-21 17:17:15 +0200

    update some stuff

--- .config/qtile/startup.sh
+++ .config/qtile/startup.sh
@@ -1,2 +1,19 @@
 #!/usr/bin/env bash
 
+### UNCOMMENT ONLY ONE OF THE FOLLOWING THREE OPTIONS! ###
+# 1. Uncomment to restore last saved wallpaper
+# xargs xwallpaper --stretch < ~/.cache/wall &
+# 2. Uncomment to set a random wallpaper on login
+# find /usr/share/backgrounds/dtos-backgrounds/ -type f | shuf -n 1 | xargs xwallpaper --stretch &
+# 3. Uncomment to set wallpaper with nitrogen
+nitrogen --set-zoom-fill \
+         "$HOME/.local/share/backgrounds/samuel-ferrara-uOi3lg8fGl4-unsplash.jpg" &
+
+if [ "$(hostname)" = "ABT-C-002NY" ]
+then
+    # Use this widget to create vpn session
+    osd-toolkit &
+fi
+
+# Network manager applet
+[ -x "$(command -v nm-applet)" ] && nm-applet &
