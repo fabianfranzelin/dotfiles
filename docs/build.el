7f58d1135c039f53d69f562c36ed8532bd872694
Franzelin Fabian (XC-DX/ESB4) <fabian.franzelin@de.bosch.com>
2024-06-25 15:04:35 +0200

    update some stuff

--- docs/build.el
+++ docs/build.el
@@ -1,9 +1,14 @@
 #!/usr/bin/emacs -x
 
+;;; build.el --- Build the org notes into HTML files
+;;; Commentary:
+
+;;; Code:
+
 ;; Set the package installation directory so that packages aren't stored in the
 ;; ~/.emacs.d/elpa path.
 (require 'package)
-(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-user-dir (expand-file-name ".packages-org-html-export" (temporary-file-directory)))
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
@@ -18,20 +23,34 @@
 ;; Load the publishing system
 (require 'ox-publish)
 
-;; Customize the HTML output
+;; Customize the HTML output:
+;; org-html-head is taken from the setup file for readtheorg theme of https://github.com/fniessen/org-html-themes/tree/master
+;; In order to apply it for all html files, I added it here as general header
+
+(defun ff/file-to-string (file)
+  "File to string function.
+FILE: file to be loaded and converted to string."
+  (with-temp-buffer
+    (insert-file-contents file)
+    (buffer-string)))
+
+(defvar ff/theme-html-head (ff/file-to-string (expand-file-name "themes/readtheorg.html" default-directory)))
+
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+      org-html-head ff/theme-html-head)
 
 ;; Define the publishing project
+(defvar ff/base-dir "./content")
+(defvar ff/public-dir "./public")
 (setq org-publish-project-alist
       (list
        (list "org-site:main"
              :recursive t
-             :base-directory "./content"
+             :base-directory ff/base-dir
              :publishing-function 'org-html-publish-to-html
-             :publishing-directory "./public"
+             :publishing-directory ff/public-dir
              :with-author nil           ;; Don't include author name
              :with-creator t            ;; Include Emacs and Org versions in footer
              :with-toc t                ;; Include a table of contents
@@ -42,3 +61,6 @@
 (org-publish-all t)
 
 (message "Build complete!")
+
+(provide 'build.el)
+;;; build.el ends here
