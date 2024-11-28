a1a988f80d65734bf245ef8124250c19e8a74729
Franzelin Fabian (XC-DX/ESB4) <fabian.franzelin@de.bosch.com>
2024-11-28 11:09:22 +0100

    update some stuff

--- .config/emacs/lisp/ff-ai-assistant.el
+++ .config/emacs/lisp/ff-ai-assistant.el
@@ -18,7 +18,7 @@
   :custom
   (gptel-api-key #'ff/get-openai-token)
   (gptel-default-mode 'org-mode)
-  (gptel-model "gpt-3.5-turbo")
+  (gptel-model 'gpt-4o-mini)
   :config
   ;; auto scroll as ChatGPT provides new responses
   (add-hook 'gptel-post-stream 'gptel-auto-scroll)
