0bbe9041b09d6117a2ce959f3e9da1d04c74ab10
Fabian Franzelin <fabian.franzelin@gmail.com>
2025-01-17 17:27:22 +0100

    update some stuff

--- .config/minimal-emacs/init.el
+++ .config/minimal-emacs/init.el
@@ -316,9 +316,6 @@ DIR: directory"
   :preface (ff/vc-install :name "savehist")
   :init (savehist-mode t))
 
-(use-package run-command
-  :custom (run-command-default-runner #'run-command-runner-compile))
-
 ;; -------------------------------------------------------------------
 (use-package dired
   :ensure nil
