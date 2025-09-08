cbb489ccd2b3e48e2b11344fd292d658f23ee58f
Fabian Franzelin <fabian.franzelin@gmail.com>
2025-09-08 22:57:35 +0200

    feat: update some stuff

--- .config/emacs/lisp/ff-programming-python.el
+++ .config/emacs/lisp/ff-programming-python.el
@@ -35,24 +35,43 @@
 ;; delete output buffer on buffer execution
 (setq py-shell-switch-buffers-on-execute nil)
 
+;; configure a few lsp server alternatives
+(with-eval-after-load 'eglot
+  (add-to-list 'eglot-server-programs
+               `(python-base-mode
+                 . ,(eglot-alternatives '(("pylsp")
+                                          ("basedpyright-langserver" "--stdio")
+                                          ("ty" "server")
+                                          ("pyright-langserver" "--stdio"))))))
+
+
 ;; configure auto format
 (with-eval-after-load 'apheleia
-  (add-hook 'python-ts-mode-hook 'apheleia-mode)
+  (add-hook 'python-base-mode-hook 'apheleia-mode)
   (setf (alist-get 'black apheleia-formatters) '("black" "-"))
   (setf (alist-get 'isort apheleia-formatters) '("isort" "--stdout" "-"))
-  (setf (alist-get 'python-ts-mode apheleia-mode-alist) '(isort black)))
+  (setf (alist-get 'python-base-mode apheleia-mode-alist) '(isort black)))
 
 ;; supports virtual environments. To be set with pyvenv-workon
 (use-package pyvenv
+  :preface
+  (defun ff/python-venv-init ()
+    (when-let* ((project (project-current))
+                (project-root (project-root project))
+                (venv-path (expand-file-name ".venv" project-root)))
+      (when (file-directory-p venv-path)
+        (make-local-variable 'pyvenv-virtual-env)
+        (pyvenv-activate venv-path))))
   :hook
   (after-init . pyvenv-mode)
-  (after-init . pyvenv-tracking-mode))
+  (after-init . pyvenv-tracking-mode)
+  (python-base-mode . ff/python-venv-init))
 
 ;; enable sphinx doc strings support
 ;; C-c M-d sphinx-doc
 (use-package sphinx-doc
   :hook
-  (python-ts-mode . sphinx-doc-mode))
+  (python-base-mode . sphinx-doc-mode))
 
 ;; magit like interface to pytest
 (use-package python-pytest
@@ -65,15 +84,15 @@
     "-x"
     '("-v" "increase verbosity" "-vvv"))
   :bind
-  (:map python-ts-mode-map
+  (:map python-base-mode-map
         ("C-c t d" . python-pytest-dispatch)
         ("C-c t l" . python-pytest-function)
         ("C-c t f" . python-pytest-file-dwim)
         ("C-c t e" . python-pytest-last-failed)))
 
 ;; flycheck
-(flycheck-add-mode 'python-pylint 'python-ts-mode)
-(flycheck-add-mode 'python-mypy 'python-ts-mode)
+(flycheck-add-mode 'python-pylint 'python-base-mode)
+(flycheck-add-mode 'python-mypy 'python-base-mode)
 
 (provide 'ff-programming-python)
 
