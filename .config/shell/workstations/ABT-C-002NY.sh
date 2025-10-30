5bda3fa1c171df49981761bafcc6aabaf01f88e7
Franzelin Fabian (XC-AS/ESS4) <fabian.franzelin@de.bosch.com>
2025-10-30 10:39:27 +0100

    feat: update some stuff

--- .config/shell/workstations/ABT-C-002NY.sh
+++ .config/shell/workstations/ABT-C-002NY.sh
@@ -14,9 +14,16 @@ export HTTPS_PROXY="http://127.0.0.1:3128"
 # Set default browser use `xdg-open` to call it
 export BROWSER=firefox
 
-# initialize default setup
-# alias init='autorandr --load gottenheim; set_keyboard_layout; unlock_key; osd-vpn-connect -k; emacs --daemon'
-alias init='autorandr --load pauline; set_keyboard_layout; unlock_key; osd-vpn-connect -k'
+# initialize default setup according to the available wifis
+current_wifi=$(nmcli dev wifi list)
+
+if echo "$current_wifi" | grep -q "Tomate"; then
+    alias init='autorandr --load gottenheim; set_keyboard_layout; unlock_key; osd-vpn-connect -k'
+elif echo "$current_wifi" | grep -q "Pauline"; then
+    alias init='autorandr --load pauline; set_keyboard_layout; unlock_key; osd-vpn-connect -k'
+else
+    alias init='set_keyboard_layout; unlock_key; osd-vpn-connect -k'
+fi
 
 #------------------------------------------------------------------------------#
 # Azure DevOps
