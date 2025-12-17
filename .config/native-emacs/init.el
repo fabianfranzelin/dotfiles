4ecffae13f200e2e579117f9bf055cb15cb99f77
Franzelin Fabian (XC-AS/ESS4) <fabian.franzelin@de.bosch.com>
2025-12-17 14:24:05 +0100

    feat: update some stuff

---
+++ .config/native-emacs/init.el
@@ -0,0 +1,324 @@
+;;; init.el --- this file starts here
+
+;;; Commentary:
+;;; Code:
+
+(require 'package)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")))
+
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(require 'use-package)
+;; Automatically install packages
+(customize-set-variable 'use-package-always-ensure t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                 Core                ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq user-full-name "Fabian Franzelin"
+      user-mail-address "fabian.franzelin@
+gmail.com"
+      inhibit-startup-echo-area-message (getenv "USER"))
+
+;; enables local variables per default
+(customize-set-variable 'enable-local-variables t)
+
+;; dont warn for following symlinked files
+(setq vc-follow-symlinks t)
+
+;; disable lockfiles
+(setq create-lockfiles nil)
+
+;; define alias for yes-or-no decision
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Support wheel mouse scrolling
+(mouse-wheel-mode t)
+
+;; set unicode encoding
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+
+;; Iterate through CamelCase
+(global-subword-mode t)
+
+;; enable visual line mode to truncate long line
+(global-visual-line-mode t)
+
+;; enable auto pair mode globally
+(electric-pair-mode t)
+(electric-indent-mode nil)
+
+(keymap-global-set "M-o" 'other-window)
+
+(require 'repeat)
+(repeat-mode 1)
+
+;; https://www.emacswiki.org/emacs/Repeatable
+(defun ff/repeat-command (command)
+  "Repeat COMMAND."
+  (let ((repeat-previous-repeated-command  command)
+        (repeat-message-function           #'ignore)
+        (last-repeatable-command           'repeat))
+    (repeat nil)))
+
+;; make the undo/redo functions repeatable
+(tab-bar-history-mode 1)
+(setq tab-bar-history-limit 100)
+(keymap-global-set "C-c p" #'(lambda () (interactive) (ff/repeat-command 'tab-bar-history-back)))
+(keymap-global-set "C-c n" #'(lambda () (interactive) (ff/repeat-command 'tab-bar-history-forward)))
+
+;; Kill this buffer, instead of prompting for which one to kill
+(defun ff/kill-buffer-current ()
+  "Kill current buffer."
+  (interactive)
+  (kill-buffer (current-buffer)))
+
+(keymap-global-set "C-x k" #'(lambda ()
+                               (interactive)
+                               (ff/repeat-command 'ff/kill-buffer-current)))
+
+;; save cursor position in files even when buffers are killed
+(save-place-mode)
+
+;; -------------------------------------------------------------------
+;; Credential management
+;; -------------------------------------------------------------------
+(require 'auth-source-pass)
+(auth-source-pass-enable)
+
+(require 'auth-source)
+;; Do not allow unencrypted auth-sources. Use GPG
+(setq auth-sources `(password-store ,(expand-file-name "~/.password-store/authinfo.gpg")))
+;; Use the Emacs minibuffer for GPG pinentry
+;; https://vxlabs.com/2021/03/21/gnupg-pinentry-via-the-emacs-minibuffer/
+(setq epa-pinentry-mode 'loopback)
+
+;; (use-package pass
+;;   :preface (ff/vc-install :repo "NicolasPetton" :name "pass")
+;;   :custom (pass-show-keybindings nil))
+
+(defun ff/unlock-key ()
+  "Unlock gpg key."
+  (interactive)
+  (if (password-store-get "usernames/public@github")
+      (message "GPG key is unlocked")
+    (message "Wrong password. GPG key is not unlocked.")))
+
+(keymap-global-set "C-c C-g" 'ff/unlock-key)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                 GUI                 ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; disable scrollbar
+(when (display-graphic-p)
+  (scroll-bar-mode -1)
+  (menu-bar-mode -1)
+  (tool-bar-mode -1))
+
+;; set cursor style to filled bar
+(setq-default cursor-type 'box)
+
+;; no splash screen
+(setq inhibit-startup-screen t)
+
+;; -------------------------------------------------------------------
+
+(defun ff/minibuffer-backward-kill (arg)
+  "Delete parent folder completely when hitting it with the cursor.
+ARG: position"
+  (interactive "p")
+  (if minibuffer-completing-file-name
+      (progn
+        ;; Expand the home path when hitting ~/ and continue
+        (when (string-match-p "^~/$" (minibuffer-contents))
+          (delete-minibuffer-contents)
+          (insert (expand-file-name "~/")))
+
+        ;; Expand home path when hitting tilde with tramp
+        (when (string-match-p "^/.*:.*:~/$" (minibuffer-contents))
+          (zap-up-to-char (- arg) ?~)
+          (delete-char -1)
+          (insert (expand-file-name "~/")))
+
+        ;; Continue
+        (cond ((string-match-p "^/.*:.*:.*/.*/$" (minibuffer-contents))
+               (zap-up-to-char (- arg) ?/))
+              ((string-match-p "^/.*:.*:.*/$" (minibuffer-contents))
+               (zap-up-to-char (- (+ arg 1)) ?:))
+              ((string-match-p "^/.*:.*:.*/$" (minibuffer-contents))
+               (zap-up-to-char (- arg) ?:))
+              ((string-match-p "^/.*:$" (minibuffer-contents))
+               (zap-up-to-char (- arg) ?/))
+              ((or (string-match-p "^/.*/$" (minibuffer-contents))
+                   (string-match-p "^~/.*/$" (minibuffer-contents)))
+               (zap-up-to-char (- arg) ?/))
+              (t ;; default
+               (delete-char -1))))
+    (delete-char -1)))
+
+(defun ff/minibuffer-move-to-dir (dir)
+  "Go the the specified directory when completing file names.
+DIR: directory"
+  (interactive "p")
+  (cond (minibuffer-completing-file-name
+         (delete-minibuffer-contents)
+         (insert (substitute-in-file-name dir)))
+        (t
+         (move-beginning-of-line 0))))
+
+;; vertical completion
+(fido-vertical-mode t)
+
+;; Enable fuzzy matching
+(setq completion-styles '(flex basic partial-completion emacs22))
+(setq completion-category-defaults nil)
+(setq completion-category-overrides '((file (styles partial-completion))))
+
+;; Configure TAB for selection in minibuffer
+(define-key minibuffer-local-map "<tab>" 'icomplete-fido-ret)
+(define-key minibuffer-local-map "C-a" (lambda() (interactive) (ff/minibuffer-move-to-dir "/")))
+(define-key minibuffer-local-map "C-o" (lambda() (interactive) (ff/minibuffer-move-to-dir "~/")))
+(define-key minibuffer-local-map "C-l" ff/minibuffer-backward-kill)
+
+;; -------------------------------------------------------------------
+(use-package dired
+  :ensure nil
+  :custom
+  (dired-auto-revert-buffer nil) ; Auto update when buffer is revisited
+  (dired-dwim-target t)
+  (dired-recursive-deletes 'always)
+  (dired-recursive-copies 'always)
+  (delete-by-moving-to-trash t)
+  (dired-listing-switches "-agho --group-directories-first")
+  (dired-hide-details-hide-symlink-targets nil)
+  :init
+  (setq dired-vc-rename-file t)
+  (require 'dired-x)
+  (autoload 'dired-omit-mode "dired-x")
+  :hook
+  (dired-mode . auto-revert-mode)
+  (dired-mode . dired-hide-details-mode)
+  (dired-mode . hl-line-mode)
+  :bind
+  (("C-x C-j" . dired-jump)
+   :map dired-mode-map
+   ("l" . dired-up-directory)
+   ("TAB" . dired-find-file)))
+
+;; -------------------------------------------------------------------
+;; Tramp
+(use-package tramp
+  :ensure nil
+  :custom
+  (tramp-terminal-type "dumb")
+  (tramp-default-method "ssh")
+  (tramp-use-ssh-controlmaster-options nil)
+  (vc-handled-backends '(Git))
+  (tramp-verbose 3)
+  ;; (tramp-auto-save-directory (expand-file-name "tramp/backups" no-littering-var-directory))
+  ;; (tramp-persistency-file-name (expand-file-name "tramp/persistency_data" no-littering-var-directory))
+  ;; make sure vc stuff is not making tramp slower
+  (vc-ignore-dir-regexp (format "%s\\|%s"
+		                vc-ignore-dir-regexp
+		                tramp-file-name-regexp))
+  ;; https://www.gnu.org/software/emacs/manual/html_node/tramp/Password-handling.html
+  (ange-ftp-netrc-filename (expand-file-name "authinfo.gpg" (getenv "PASSWORD_STORE_DIR"))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;           Version control           ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;           Project                  ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package project
+  :ensure nil
+  :custom
+  (project-switch-commands '((project-find-file "Find file")
+                             (project-find-dir "Find directory")
+                             ;; (ff/project-magit "Magit")
+                             (project-dired "Dired")
+                             (project-vc-dir "VC-Dir")))
+  (project-vc-extra-root-markers '(".project.el"))
+  (project-vc-ignores '("build/" "install/" ".*cache/" "__pycache__"))
+  :bind
+  (:map project-prefix-map
+        ("g" . ff/project-magit)
+        ("v" . project-vc-dir)
+        ("C" . run-command)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;           org-mode                  ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'org)
+(customize-set-variable
+ 'org-confirm-babel-evaluate nil)
+
+;; for some reason, this is needed to make org-mode work
+(customize-set-variable
+ 'org-directory "~/workspace/org")
+(customize-set-variable
+ 'org-agenda-files `(,(expand-file-name "notes" org-directory)
+                     ,(expand-file-name "notes/journal" org-directory)))
+
+;; Todo types
+;;     TODO - A task that should be done at some point
+;;     NEXT - This task should be done next (in the Getting Things Done sense)
+;;     WAIT - Waiting for someone else to be actionable again
+;;     DONE - It's done!
+(customize-set-variable
+ 'org-todo-keywords
+ '((sequence "TODO(t)" "NEXT(n)" "WAIT(w)" "|" "DONE(d!)" "DISCARD(c)")))
+
+(customize-set-variable
+ 'org-todo-keyword-faces
+ '(("NEXT" . (:foreground "orange red" :weight bold))
+   ("WAIT" . (:foreground "HotPink2" :weight bold))))
+
+;; Customize org-agenda
+(customize-set-variable 'org-agenda-window-setup 'current-window)
+(customize-set-variable 'org-agenda-span 'week)
+(customize-set-variable 'org-agenda-start-with-log-mode t)
+
+;; Make done tasks show up in the agenda log
+(customize-set-variable 'org-log-done 'time)
+(customize-set-variable 'org-log-into-drawer t)
+
+(customize-set-variable
+ 'org-columns-default-format
+ "%20CATEGORY(Category) %65ITEM(Task) %TODO %6Effort(Estim){:}  %6CLOCKSUM(Clock) %TAGS")
+
+(customize-set-variable
+ 'org-agenda-custom-commands
+ `(("d" "Dashboard"
+    ((agenda "" ((org-deadline-warning-days 14)))
+     (todo "NEXT"
+           ((org-agenda-overriding-header "Next Actions")
+            (org-agenda-max-todos nil)))
+     (todo "WAIT"
+           ((org-agenda-overriding-header "Waiting for Action")
+            (org-agenda-max-todos nil)))
+     (todo "TODO"
+           ((org-agenda-overriding-header "Unprocessed Inbox Tasks")
+            (org-agenda-files '(,(expand-file-name "notes/journal/inbox.org" org-directory)))
+            (org-agenda-text-search-extra-files nil)))
+     (alltodo "")))
+   ("n" "Next Tasks"
+    ((agenda "" ((org-deadline-warning-days 7)))
+     (todo "NEXT"
+           ((org-agenda-overriding-header "Next Tasks")))))
+   ("A" "Agenda and all TODOs"
+    ((agenda "")
+     (alltodo "")))))
+
+(provide 'init)
+
+;;; init.el ends here
