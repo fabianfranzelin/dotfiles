7a1ea7c1b5a72099998a1e441812e663027e59c6
Fabian Franzelin <fabian.franzelin@gmail.com>
2026-01-08 15:37:48 +0100

    feat: update some stuff

--- .config/native-emacs/init.el
+++ .config/native-emacs/init.el
@@ -265,6 +265,26 @@ DIR: directory"
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;           Version control           ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; This is what you probably want if you are using a tiling window
+;; manager under X, such as ratpoison.
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+(setq ediff-split-window-function 'split-window-horizontally)
+
+;; save and restore window configuration when entering and quitting
+;; ediff
+;; https://www.emacswiki.org/emacs/EdiffMode
+(add-hook 'ediff-load-hook
+          (lambda ()
+
+            (add-hook 'ediff-before-setup-hook
+                      (lambda ()
+                        (setq ediff-saved-window-configuration (current-window-configuration))))
+
+            (let ((restore-window-configuration
+                   (lambda ()
+                     (set-window-configuration ediff-saved-window-configuration))))
+              (add-hook 'ediff-quit-hook restore-window-configuration 'append)
+              (add-hook 'ediff-suspend-hook restore-window-configuration 'append))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;           Project                  ;
