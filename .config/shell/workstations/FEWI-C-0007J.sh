a10b172f487784bbb8e2f23af1108ae9d73da9b2
Franzelin Fabian (XC-AS/ESS4) <fabian.franzelin@de.bosch.com>
2026-03-26 22:23:50 +0100

    add new workstation

---
+++ .config/shell/workstations/FEWI-C-0007J.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+# shellcheck disable=SC1090
+
+# Load specific settings for Bosch
+
+#------------------------------------------------------------------------------#
+# Dotfiles setup
+export __SHELL_LIB="${XDG_CONFIG_HOME}/shell"
+
+export HTTP_PROXY="http://127.0.0.1:3128"
+export HTTPS_PROXY="http://127.0.0.1:3128"
+
+# Set default browser use `xdg-open` to call it
+export BROWSER=firefox
+
+# initialize default setup according to the available wifis
+AVAILABLE_WIFIS=$(nmcli dev wifi list)
+
+if echo "${AVAILABLE_WIFIS}" | grep -q "Tomate"; then
+    alias init='autorandr --load gottenheim; set_keyboard_layout; unlock_key; osd-vpn-connect -k'
+elif echo "${AVAILABLE_WIFIS}" | grep -q "Pauline"; then
+    alias init='autorandr --load pauline; set_keyboard_layout; unlock_key; osd-vpn-connect -k'
+elif echo "${AVAILABLE_WIFIS}" | grep -q "BWSOA"; then
+    alias init='autorandr --load feuerbach; set_keyboard_layout; unlock_key'
+else
+    alias init='set_keyboard_layout; unlock_key; osd-vpn-connect -k'
+fi
+
+#------------------------------------------------------------------------------#
+# Azure DevOps
+# Run  cat BOSCH-CA-DE_pem.cer /opt/az/lib/python3.6/site-packages/certifi/cacert.pem > azure-bosch-cert.pem
+# export REQUESTS_CA_BUNDLE="${HOME}/.local/share/certificates/bosch/azure-bosch-cert.pem"
