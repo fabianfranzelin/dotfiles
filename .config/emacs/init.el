892efa0b546309d3b86b4572fbbb84625b7be098
Fabian Franzelin <fabian.franzelin@gmail.com>
2026-09-24 00:47:46 +0200

    feat: update some stuff

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -19,7 +19,7 @@
 (require 'ff-spellcheck)
 (require 'ff-ai-assistant)
 (require 'ff-multimedia)
-(require 'ff-messaging)
+;; (require 'ff-messaging)
 (require 'ff-setup-gui)
 ;; -------------------------------------------------------------------
 
